8309193db319ddcafb8d375da7ef4f3dfa5b1f47
Jairo Antonio Paniagua Garcia <jpaniagua@isa1890.com>
2026-06-03 09:59:55 -0600

    agregamos proyecto

---
+++ app/src/main/java/com/nsel/testcompose/MainActivity.kt
@@ -0,0 +1,195 @@
+package com.nsel.testcompose
+
+import android.content.res.Configuration
+import android.graphics.Paint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CheckboxDefaults.colors
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.CircularWavyProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.ImeOptions
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.nsel.testcompose.ui.theme.TestComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TestComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        LoginScreen()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun LoginScreen(){
+    var uName by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isLoading by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+
+        Image(
+            painter = painterResource(id = R.drawable.fondo_ala),
+            contentDescription = "Background Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
+            modifier = Modifier.align(Alignment.Center)
+                .padding(horizontal = 30.dp)
+                .fillMaxWidth()
+                .alpha(0.8f)
+                .background(color = MaterialTheme.colorScheme.surface, shape = RoundedCornerShape(16.dp))
+                .padding(vertical = 30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(id= R.drawable.logo_isa),
+                contentDescription = "Logo",
+                modifier = Modifier.fillMaxWidth()
+                    .height(100.dp)
+                    .padding(bottom = 20.dp)
+            )
+
+            TextField(
+                value = uName,
+                onValueChange = { uName = it},
+                placeholder = { Text("Usuario", color = MaterialTheme.colorScheme.onSurface, fontSize = 15.sp) },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier.fillMaxWidth()
+                    .padding(horizontal = 30.dp)
+                    .height(56.dp)
+            )
+
+            Spacer(modifier = Modifier.height(22.dp))
+
+            TextField(
+                value = password,
+                onValueChange = { password = it},
+                placeholder = { Text("Contraseña", color= MaterialTheme.colorScheme.onSurface, fontSize = 15.sp)},
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier.fillMaxWidth()
+                    .height(56.dp)
+                    .padding(horizontal = 30.dp)
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Button(
+                onClick = {
+                    isLoading = true
+                },
+                modifier = Modifier.fillMaxWidth()
+                    .height(50.dp)
+                    .padding(horizontal = 30.dp),
+                enabled = !isLoading,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                if(isLoading){
+                    CircularWavyProgressIndicator(
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.size(24.dp)
+                    )
+                }else{
+                    Text(text = "Iniciar Sesión", color = MaterialTheme.colorScheme.onPrimary, fontSize = 17.sp)
+                }
+            }
+
+        }
+    }
+}
+
+
+@Preview(
+    name = "Modo Claro",
+    showBackground = true,
+    device = Devices.PHONE
+)
+@Preview(
+    name = "Modo Oscuro",
+    showBackground = true,
+    device = Devices.PHONE,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun FullScreenPreview(){
+    TestComposeTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                LoginScreen()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/nsel/testcompose/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.nsel.testcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val backpLight = Color(0xFFFFFFFF)
+val textLight = Color(0xFF8D8D8D)
+val backsLight = Color(0xFFF6F6F6)
+val textButtonLight = Color(0xFFFFFFFF)
+val buttonLight = Color(0xFF32A522)
+
+val backpDark = Color(0xFF090909)
+val textDark = Color(0xFFBDBDBD)
+val backsDark = Color(0xFF2F2F2F)
+val textButtonDark = Color(0xFFE7E7E7)
+val buttonDark = Color(0xFF64A859)
