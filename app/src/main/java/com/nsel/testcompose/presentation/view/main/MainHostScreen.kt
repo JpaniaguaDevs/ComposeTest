6d83c082a04a024e0092825167855a9d4629720a
Jairo Antonio Paniagua Garcia <jpaniagua@isa1890.com>
2026-06-19 09:58:58 -0600

    actualizaciones de dependencia

---
+++ app/src/main/java/com/nsel/testcompose/presentation/view/main/MainHostScreen.kt
@@ -0,0 +1,9 @@
+package com.nsel.testcompose.presentation.view.main
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MainHostScreen(){
+    val navController = rememberNavController()
+}
