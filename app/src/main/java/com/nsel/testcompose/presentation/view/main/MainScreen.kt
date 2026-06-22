5cd6cb77ec6ec9d8fe0ce78c01530ae9b3971f15
Jairo Antonio Paniagua Garcia <jpaniagua@isa1890.com>
2026-06-22 11:27:06 -0600

    definimos las pantallas que estarán dentro del padre

--- app/src/main/java/com/nsel/testcompose/presentation/view/main/MainScreen.kt
+++ app/src/main/java/com/nsel/testcompose/presentation/view/main/MainScreen.kt
@@ -1,29 +1,89 @@
 package com.nsel.testcompose.presentation.view.main
 
+import android.R
+import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nsel.testcompose.ui.theme.TestComposeTheme
 
 @Composable
 fun HomeScreen(){
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("¡Bienvenido al Inicio, llegaste desde el Login!")
+    Box(modifier = Modifier.fillMaxSize(). background(color= MaterialTheme.colorScheme.surface), contentAlignment = Alignment.Center) {
+        Text(modifier= Modifier.padding(20.dp),color = MaterialTheme.colorScheme.onSurface, text = "¡Bienvenido al Inicio!")
+    }
+}
+@Preview(
+    name = "Home Modo Claro",
+    showBackground = true,
+    device = Devices.PHONE
+)
+@Preview(
+    name = "Home Modo Oscuro",
+    showBackground = true,
+    device = Devices.PHONE,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun PreviewHomeScreen() {
+    TestComposeTheme {
+        HomeScreen()
     }
 }
 
 @Composable
 fun ProfileScreen(){
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("¡Bienvenido al Perfil, llegaste desde el Login!")
+    Box(modifier = Modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.surface), contentAlignment = Alignment.Center) {
+        Text(modifier= Modifier.padding(20.dp), color = MaterialTheme.colorScheme.onSurface, text ="¡Bienvenido al Perfil!")
+    }
+}
+@Preview(
+    name = "Profile Modo Claro",
+    showBackground = true,
+    device = Devices.PHONE
+)
+@Preview(
+    name = "Profile Modo Oscuro",
+    showBackground = true,
+    device = Devices.PHONE,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun PreviewProfileScreen() {
+    TestComposeTheme {
+        ProfileScreen()
     }
 }
 
 @Composable
 fun SettingsScreen(){
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("¡Bienvenido a Configuración, llegaste desde el Login!")
+    Box(modifier = Modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.surface), contentAlignment = Alignment.Center) {
+        Text(modifier= Modifier.padding(20.dp), color = MaterialTheme.colorScheme.onSurface, text ="¡Bienvenido a Configuración!")
+    }
+}
+@Preview(
+    name = " Settings Modo Claro",
+    showBackground = true,
+    device = Devices.PHONE
+)
+@Preview(
+    name = "Settings Modo Oscuro",
+    showBackground = true,
+    device = Devices.PHONE,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun PreviewSettingsScreen() {
+    TestComposeTheme {
+        SettingsScreen()
     }
 }
