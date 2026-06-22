4585bf2449240358f68c25e83401c2dd5d7edccf
Jairo Antonio Paniagua Garcia <jpaniagua@isa1890.com>
2026-06-22 11:27:20 -0600

    navegación de pantallas en padre

--- app/src/main/java/com/nsel/testcompose/presentation/view/main/MainHostScreen.kt
+++ app/src/main/java/com/nsel/testcompose/presentation/view/main/MainHostScreen.kt
@@ -1,9 +1,84 @@
 package com.nsel.testcompose.presentation.view.main
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.nsel.testcompose.presentation.navigation.BottomNavItem
+import com.nsel.testcompose.ui.theme.TestComposeTheme
 
 @Composable
 fun MainHostScreen(){
     val navController = rememberNavController()
+
+    val bottomNavItem = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Profile,
+        BottomNavItem.Settings
+    )
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar(
+                containerColor = MaterialTheme.colorScheme.surfaceVariant
+            ) {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.destination?.route
+
+                bottomNavItem.forEach { item ->
+                    NavigationBarItem(
+                        icon = { Icon(imageVector =  item.icon, contentDescription = item.title) },
+                        label = { Text(text = item.title) },
+                        selected = currentRoute == item.route,
+                        onClick = {
+                            navController.navigate(item.route){
+                                popUpTo(navController.graph.findStartDestination().id){
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) {innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = BottomNavItem.Home.route,
+            modifier= Modifier.padding(innerPadding).fillMaxSize()
+        ){
+            composable(BottomNavItem.Home.route) { HomeScreen() }
+            composable(BottomNavItem.Profile.route) { ProfileScreen() }
+            composable(BottomNavItem.Settings.route) { SettingsScreen() }
+        }
+    }
+}
+
+@Preview(name = "Main Host (Padre) - Modo Claro", showBackground = true)
+@Preview(
+    name = "Main Host (Padre) - Modo Oscuro",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun PreviewMainHostScreen() {
+    TestComposeTheme {
+        MainHostScreen()
+    }
 }
