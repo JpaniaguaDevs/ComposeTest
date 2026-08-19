bced7c1d8217099d7389ff38511953c16f000bd8
Jairo Antonio Paniagua García <jpaniagua@isa1890.com>
2026-08-19 16:22:07 -0600

    change

--- app/src/main/java/com/nsel/testcompose/domain/model/Client.kt
+++ app/src/main/java/com/nsel/testcompose/domain/model/Client.kt
@@ -1,3 +1,6 @@
 package com.nsel.testcompose.domain.model
 
-data class Client(val name: String, val phone: String)
+data class Client(
+    val name: String,
+    val phone: String
+)
--- app/src/main/java/com/nsel/testcompose/domain/model/Invoice.kt
+++ app/src/main/java/com/nsel/testcompose/domain/model/Invoice.kt
@@ -1,3 +1,7 @@
 package com.nsel.testcompose.domain.model
 
-data class Invoice(val detail: String, val amount: Double, val isPaid: Boolean)
+data class Invoice(
+    val detail: String,
+    val amount: Double,
+    val isPaid: Boolean
+)
